55018a88564ae85f17ace1aec0de5857d54b3e0f
BRUCEBANNER00 <105909158+BRUCEBANNER00@users.noreply.github.com>
2022-09-01 17:32:05 +0530

    Add files via upload

---
+++ Bot/__init__.py
@@ -0,0 +1,32 @@
+from .Utils import (
+   get_filter_results,
+   get_file_details,
+   is_subscribed,
+   get_poster,
+   Media
+)
+from .Channel import (
+   RATING,
+   GENRES
+)
+
+HELP = """
+Basic Commads
+
+/start Check I am alive
+
+/help To get this message
+
+/about About me
+
+"""
+
+ABOUT = """
+⭕ Name :  Neha Singh
+    
+⭕ Creator : @PRO_Botzz
+
+⭕ Language : Python3
+
+⭕ Library : Pyrogram Asyncio 1.13.0
+"""
